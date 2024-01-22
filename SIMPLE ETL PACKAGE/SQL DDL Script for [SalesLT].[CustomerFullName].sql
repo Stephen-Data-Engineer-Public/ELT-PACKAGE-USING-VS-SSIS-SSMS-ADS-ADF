3b45e5e14719ad41a27864e581d00646f838cdbc
stephen nwoye <resgad.inc@gmail.com>
2024-01-22 23:31:47 +0000

    no commit

---
+++ SIMPLE ETL PACKAGE/SQL DDL Script for [SalesLT].[CustomerFullName].sql
@@ -0,0 +1,19 @@
+CREATE TABLE [SalesLT].[CustomerFullName] (
+
+    [CustomerID] int,
+
+    [NameStyle] bit,
+
+    [Title] nvarchar(8),
+
+    [FirstName] nvarchar(50),
+
+    [MiddleName] nvarchar(50),
+
+    [LastName] nvarchar(50),
+
+    [Suffix] nvarchar(10),
+
+    [FullName] nvarchar(350)
+
+)
